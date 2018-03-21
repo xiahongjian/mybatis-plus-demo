487cc4f09c736057efe76bfd92563c36fc4f9cb2
xiahongjian <hongjian.xia@qq.com>
2018-03-21 09:03:12 +0800

    druid使用自动配置

--- src/main/java/com/example/mybatis_plus_demo/web/UserController.java
+++ src/main/java/com/example/mybatis_plus_demo/web/UserController.java
@@ -1,9 +1,15 @@
 package com.example.mybatis_plus_demo.web;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.baomidou.mybatisplus.plugins.Page;
+import com.example.mybatis_plus_demo.entity.User;
+import com.example.mybatis_plus_demo.service.UserService;
 
 /**
  * <p>
@@ -14,8 +20,15 @@ import org.springframework.stereotype.Controller;
  * @since 2018-03-19
  */
 @Controller
-@RequestMapping("/mybatis_plus_demo/user")
+@RequestMapping("/user")
 public class UserController {
-
+	@Autowired
+	private UserService userService;
+	
+	@ResponseBody
+	@GetMapping("")
+	public Page<User> findUsers() {
+		return userService.selectPage(new Page<>(0, 10));
+	}
 }
 
